37271242d58f962d24472f2924c35149108cbf16
PURVA <1032210421@tcetmumbai.in>
2024-07-25 00:35:13 +0530

    again

--- routes/url.js
+++ routes/url.js
@@ -14,7 +14,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const shortid_1 = __importDefault(require("shortid"));
-const url_1 = __importDefault(require("../models/url"));
+const url_1 = __importDefault(require("../../models/url"));
 const router = (0, express_1.Router)();
 router.post('/shorten', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const { originalUrl } = req.body;
